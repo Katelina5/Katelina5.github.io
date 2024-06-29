fd44300c8d253384e1f5e890c91e45ed7dcffdcb
molodoyzxc <semenov.svin@gmail.com>
2024-06-29 21:44:08 +0300

    secrets

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAeIQI7-YLdcpz9j7dj84aMmLTP80W3R50",
-    authDomain: "test-625b9.firebaseapp.com",
-    projectId: "test-625b9",
-    storageBucket: "test-625b9.appspot.com",
-    messagingSenderId: "652111460325",
-    appId: "1:652111460325:web:5f0b319907429b6770a10c",
-    measurementId: "G-1BM530WDZ6"
-};
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+    apiKey: window.env.FIREBASE_API_KEY,
+    authDomain: window.env.FIREBASE_AUTH_DOMAIN,
+    projectId: window.env.FIREBASE_PROJECT_ID,
+    storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: window.env.FIREBASE_APP_ID
+});
 
 let mybutton = document.getElementById("scrollToTopBtn");
 
@@ -27,13 +24,11 @@ function scrollToTop() {
     document.documentElement.scrollTop = 0;
 }
 
-// Function to dynamically add images to the gallery
 function loadImages() {
     const storage = firebase.storage();
     const storageRef = storage.ref();
     const gallery = document.getElementById('gallery');
 
-    // List all files in the storage bucket
     storageRef.listAll().then(function(result) {
         const images = result.items.map(item => item.getDownloadURL());
         return Promise.all(images);
@@ -55,5 +50,4 @@ function loadImages() {
     });
 }
 
-// Load images when the page loads
 window.onload = loadImages;
